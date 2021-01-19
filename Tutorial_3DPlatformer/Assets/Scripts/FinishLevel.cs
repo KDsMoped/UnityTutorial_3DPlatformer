dac0347ad53d2c6fb2bc14ecbebecb6448c351b1
Dominik Köhler <dominik.koehler90@gmail.com>
2021-01-19 16:21:23 +0100

    section 8
    add finish area

---
+++ Tutorial_3DPlatformer/Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLevel : MonoBehaviour
+{
+    public GameObject LevelMusic;
+    public AudioSource LevelComplete;
+    public GameObject LevelTimer;
+
+
+    void OnTriggerEnter()
+    {
+        LevelMusic.SetActive(false);
+        LevelTimer.SetActive(false);
+        LevelComplete.Play();
+    }
+}
---
+++ Tutorial_3DPlatformer/Assets/Scripts/PlatformGripper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGripper : MonoBehaviour
+{
+    public GameObject Ledge;
+    public GameObject Player;
+
+    void OnTriggerEnter()
+    {
+        Player.transform.parent = Ledge.transform;
+    }
+
+    void OnTriggerExit()
+    {
+        Player.transform.parent = null;
+    }
+}
