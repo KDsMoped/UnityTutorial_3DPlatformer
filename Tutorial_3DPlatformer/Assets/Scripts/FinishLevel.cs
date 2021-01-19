9a734fdc28bc09db914bef99fd8c8066228b551b
Dominik Köhler <dominik.koehler90@gmail.com>
2021-01-19 16:53:05 +0100

    section 9
    add death UI
    add finish UI

--- Tutorial_3DPlatformer/Assets/Scripts/FinishLevel.cs
+++ Tutorial_3DPlatformer/Assets/Scripts/FinishLevel.cs
@@ -7,12 +7,30 @@ public class FinishLevel : MonoBehaviour
     public GameObject LevelMusic;
     public AudioSource LevelComplete;
     public GameObject LevelTimer;
-
+    public GameObject TimeLeft;
+    public GameObject Score;
+    public GameObject TotalScore;
 
     void OnTriggerEnter()
     {
         LevelMusic.SetActive(false);
         LevelTimer.SetActive(false);
         LevelComplete.Play();
+
+        StartCoroutine(CalculateScore());
+    }
+
+    IEnumerator CalculateScore()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        TimeLeft.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+
+        Score.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+
+        TotalScore.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
     }
 }
--- Tutorial_3DPlatformer/Assets/Scripts/LevelDeath.cs
+++ Tutorial_3DPlatformer/Assets/Scripts/LevelDeath.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class LevelDeath : MonoBehaviour
 {
+    public GameObject YouFellText;
+    public GameObject LevelAudio;
+    public GameObject FadeOut;
+
     void OnTriggerEnter()
     {
+        StartCoroutine(HandleDeath());
+    }
+
+    IEnumerator HandleDeath()
+    {
+        YouFellText.SetActive(true);
+        LevelAudio.SetActive(false);
+        yield return new WaitForSeconds(2);
+        FadeOut.SetActive(true);
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(0);
     }
 }
