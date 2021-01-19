c77003fd9ac987498b2b20d4d4c14aad47d60cf5
Dominik Köhler <dominik.koehler90@gmail.com>
2021-01-19 14:45:04 +0100

    section 5
    add new skybox
    tweak lighting
    add simple collection of gem

---
+++ Tutorial_3DPlatformer/Assets/Scripts/GemSilver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GemSilver : MonoBehaviour
+{
+    public GameObject ScoreBox;
+    public AudioSource CollectSound;
+
+    void OnTriggerEnter()
+    {
+        ScoreBox.GetComponent<Text>().text = "1000";
+        CollectSound.Play();
+        Destroy(gameObject);
+    }
+}
