ec838d3bd3cc38c2b65c1e34ea77562bc5845cc7
Dominik Köhler <dominik.koehler90@gmail.com>
2021-01-19 15:45:59 +0100

    section 7
    add skybox rotation
    add death and level restart

---
+++ Tutorial_3DPlatformer/Assets/Scripts/RedirectToLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RedirectToLevel : MonoBehaviour
+{
+    public static int DestinationLevel = 1;
+
+    void Update()
+    {
+        if(DestinationLevel == 1)
+        {
+            SceneManager.LoadScene(DestinationLevel);
+        }
+    }
+}
